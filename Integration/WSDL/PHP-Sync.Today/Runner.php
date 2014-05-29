a5979b6a954543b2bf2de5112daa903959551e78
unknown <jakub.sutak@Comp13.corp.naseukoly.cz>
2014-05-29 14:16:10 +0200

    added php wsdl

---
+++ Integration/WSDL/PHP-Sync.Today/Runner.php
@@ -0,0 +1,115 @@
+<?php
+include ('Util.php');
+include ('Actions.php');
+
+$util = new Util();
+
+$paramsUser = array('InternalId' => $util->GetNewGuid(), 'Maintenance' => -1);
+
+$account1params = array('InternalId' => $util->GetNewGuid(),
+    				    'BelongsToUser' => $util->GetArrayValue($paramsUser, "InternalId", null),
+    				    'Username' => "user@name.com",
+    				    'Password' => "Pass_word1",
+    				    'Server' => "server",
+   		 			    'AccountAssemblyName' => "naseukolycz.universalsync.communicator.inmemory.1");
+
+$account2params = array('InternalId' => $util->GetNewGuid(),
+    				    'BelongsToUser' => $util->GetArrayValue($paramsUser, "InternalId", null),
+    				    'Username' => "user@name.com",
+    				    'Password' => "Pass_word1",
+    				    'Server' => "server",
+   		 			    'AccountAssemblyName' => "naseukolycz.universalsync.communicator.inmemory.2");
+
+$contactParams = array(
+   		 'ExternalId' => $util->GetNewGuid(),
+      	 'FullName' => "Peter Forsberg",
+         'PhoneNumberHome' => "111 111 111",
+         'PhoneNumberWork' => "222 222 222",
+         'PhoneNumberCellPhone'=> "333 333 333",
+         'EmailAddressHome'=> "email@home.com",
+         'EmailAddressWork'=> "email@work.com",
+         'Address' => "Prague, Starometska 37",
+         'PhysicalAddressHomeStreet'=>"Starometska 37", 
+         'PhysicalAddressHomeCity'=>"Prague",
+         'PhysicalAddressHomeCountry'=> "Czech Republic",
+         'PhysicalAddressHomeState'=> "CZ",
+         'PhysicalAddressHomePostalCode'=>"100 01",
+         'PhysicalAddressWorkStreet'=>null,
+   		 'PhysicalAddressWorkCountry'=>null,
+   		 'PhysicalAddressWorkState'=>null,
+   		 'PhysicalAddressWorkPostalCode'=> null,
+   		 'JobPosition'=>"CEO",
+   		 'Company'=>"Company s.r.o.",
+   		 'LastModified'=> date("Y-m-d H:i:s"),
+   		 'belongsToAccount'=>$util->GetArrayValue($account1params, "InternalId", null),
+   		 'isOriginal'=>"true",
+   		 'isDeleted'=>"false",
+   		 'relationId'=> "",
+);
+
+try {
+	$actions = new Actions();
+
+	//create Client (from WSDL)
+	$client = $actions->GetClient('http://wsdl.sync.today/DataModel.asmx?WSDL', array('trace' => 1));
+	if($client==null) throw new Exception("Failed to create Client (WSDL).");
+
+	//create user
+	$user = $actions->CreateUser($client, $paramsUser);
+	if($client==null) throw new Exception("Failed to create User.");
+	echo "Created User.\n";
+
+	//create Accounts
+	$createdAccount1 = $actions->CreateAccount($client, $account1params);
+	if($createdAccount1==null) throw new Exception("Failed to create Account1.");
+	echo "Created Account1.\n";
+
+	$createdAccount2 = $actions->CreateAccount($client, $account2params);
+	if($createdAccount2==null) throw new Exception("Failed to create Account2.");
+	echo "Created Account2.\n";
+
+	//create contacts
+	$createdContact = $actions->CreateContact($client, $contactParams);
+	if($createdContact==null) throw new Exception("Failed to create Contact.");
+	echo "Created Contact.\n";
+
+	 //SynchronizeUser
+	 $sync = $actions->SynchronizeUser($client, array('userId'=>$util->GetArrayValue($paramsUser, "InternalId", null)));
+	 if($createdContact==null) throw new Exception("Failed to SynchronizeUser.");
+	 echo "Execute SynchronizeUser method.\n";
+	  
+	 //GetContactsUpdatedSince
+	 $minutes_to_add = 60;
+	 $time = new DateTime();
+	 $time->add(new DateInterval('PT' . $minutes_to_add . 'M'));
+	 $lessHourTime = $time->format('Y-m-d H:i');
+
+	 $updatedSince = $actions->GetContactsUpdatedSince($client, array('accountId'=>$util->GetArrayValue($account1params, "InternalId", null), 'sinceDateTime'=>$lessHourTime));
+	 if($updatedSince==null || $updatedSince->GetContactsUpdatedSince2Result == null) throw new Exception("Failed to GetContactsUpdatedSince.");
+	 if(count($updatedSince->GetContactsUpdatedSince2Result) != 1) throw new Exception("WSDL method GetContactsUpdatedSince2 must return 1 but returned xyz.");
+	 echo "Execute UpdatedSince method.\n";
+	  /*
+	 $arr = $updatedSince->NuContact[0];
+	 foreach($updatedSince->GetContactsUpdatedSince2Result as $o){
+		var_dump($o->Company);
+		}
+
+		if($arr == null)
+		{
+		echo "NULL";
+		}
+		else
+		{
+		echo "NOT NULL";
+		}
+		*/
+
+
+	//if($util->GetArrayValue($updatedContact,"Company",null)!="Company s.r.o.") throw new Exception("Company in returned contact is xyz but expected was Company s.r.o.");
+
+} catch (SoapFault $e) {
+	var_dump(libxml_get_last_error());
+	var_dump($e);
+}
+
+?>
