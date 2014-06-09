cdf11b532e248197a9c9a3b8d5d23d349634bb4d
JiriLazansky <jiri.lazuansky@naseukoly.cz>
2014-06-09 16:18:23 +0200

    translation...

--- Integration/WSDL/PowerShell/CreateUser_AddAcc.ps1
+++ Integration/WSDL/PowerShell/CreateUser_AddAcc.ps1
@@ -1,8 +1,7 @@
-﻿#Dej metody
-#New-WebServiceProxy -Uri http://wsdl.sync.today/DataModel.asmx?WSDL | Get-Member -Type Method -name createuser2 |fl
+﻿#New-WebServiceProxy -Uri http://wsdl.sync.today/DataModel.asmx?WSDL | Get-Member -Type Method -name createuser2 |fl
 
 #Create new user
-Write-Host -ForegroundColor Green "<<<VYTVARIM UZIVATELE>>>"
+Write-Host -ForegroundColor Green "<<<Creating User>>>"
 
 $synctoday = New-WebServiceProxy -Uri http://wsdl.sync.today/DataModel.asmx?WSDL
 $userid = [System.Guid]::NewGuid().ToString()
@@ -13,30 +12,30 @@ $userPassword = Read-Host "Password" -AsSecureString
 $maint = "-1"
 $synctoday.CreateUser2($userid, $maint, $userName, $userLast, $userEmail, $userPassword)
 
-#Pridej Acc1 k uzivately
-Write-Host -ForegroundColor Red "<<<UCET GOOGLE>>>"
+#Add Account1 to user
+Write-Host -ForegroundColor Red "<<<GOOGLE ACCOUNT>>>"
 $acctid1 =  [System.Guid]::NewGuid().ToString()
 $accName1 = Read-Host "user@name.com"
-$password1 = Read-Host "Heslo" -AsSecureString
+$password1 = Read-Host "Password" -AsSecureString
 $server1 = Read-Host "Server"
 $AAN1 = "naseukolycz.universalsync.communicator.inmemory.1"
-Write-Host -ForegroundColor Red "<<<PRIDAVAM UCET1 $accName1 K UZIVATELY $userid>>>"
+Write-Host -ForegroundColor Red "<<<Adding $accName1 to $userName $userLast>>>"
 $synctoday.CreateAccount2($acctid1,$userid,$accName1,$password1,$server1,$AAN1)
 
-#Pridej Acc2 k uzivately
-Write-Host -ForegroundColor Cyan "<<<UCET ATOLLON>>>"
+#Add Account2 to user
+Write-Host -ForegroundColor Cyan "<<<ATOLLON ACCOUNT>>>"
 $acctid2 =  [System.Guid]::NewGuid().ToString()
 $accName2 = Read-Host "user@name.com"
-$password2 = Read-Host "Heslo" -AsSecureString
+$password2 = Read-Host "Password" -AsSecureString
 $server2 = Read-Host "Server"
 $AAN2 = "naseukolycz.universalsync.communicator.inmemory.2"
-Write-Host -ForegroundColor Cyan "<<<PRIDAVAM UCET2 $accName2 K UZIVATELY $userid>>>"
+Write-Host -ForegroundColor Cyan "<<<Adding $accName2 to $userName $userLast>>>"
 $synctoday.CreateAccount2($acctid2,$userid,$accName2,$password2,$server2,$AAN2)
 
-#kontrola
+#Check
 #$synctoday.GetAccounts($userid)
 
-#Kontorla hesla
+#Password Check
 #Param(
 #    [Parameter(Mandatory=$true, Position=0, HelpMessage="Password?")]
 #     [SecureString]$password
@@ -46,39 +45,39 @@ $synctoday.CreateAccount2($acctid2,$userid,$accName2,$password2,$server2,$AAN2)
 #
 #   write-host $pw
 
-#Vytvarim kontakt
-Write-Host -ForegroundColor Green "<<<VYTVARIM KONTAKT>>>"
+#Creating Contact
+Write-Host -ForegroundColor Green "<<<Creating Contact>>>"
 $extid = [System.Guid]::NewGuid().ToString() 
-$contactName = Read-Host "Jmeno a prijmeni"
-$PhoneNumberHome = Read-Host "Telefon domu"
-$PhoneNumberWork = Read-Host "Telefon do prace"
-$EmailAddressHome = Read-host "Email osobni"
-$EmailAddressWork = Read-Host "Email do prace"
-$PhoneNumberCellPhone = Read-Host "PhoneNumberCellPhone"
-$Address = Read-Host "Adresa domu"
-$PhysicalAddressHomeStreet = Read-Host "Ulice"
-$PhysicalAddressHomeCity = Read-Host "Mesto"
-$PhysicalAddressHomeCountry = Read-Host "Okres"
-$PhysicalAddressHomeState = Read-Host "Stat"
-$PhysicalAddressHomePostalCode = Read-Host "PSC"
-$PhysicalAddressWorkStreet = Read-Host "Ulice firmy"
-$PhysicalAddressWorkCity = Read-Host "Mesto firmy"
-$PhysicalAddressWorkCountry = Read-Host "Oblast firmy"
-$PhysicalAddressWorkState = Read-Host "Stat firmy"
-$PhysicalAddressWorkPostalCode = Read-Host "PSC firmy"
-$JobPosition = Read-Host "Pracovni pozice"
-$Company = Read-Host "Spolecnost"
+$contactName = Read-Host "Full Name"
+$PhoneNumberHome = Read-Host "Home Phone"
+$PhoneNumberWork = Read-Host "Work Phone"
+$EmailAddressHome = Read-host "Private Email"
+$EmailAddressWork = Read-Host "Work Email"
+$PhoneNumberCellPhone = Read-Host "Mobile"
+$Address = Read-Host "Home Address"
+$PhysicalAddressHomeStreet = Read-Host "Street"
+$PhysicalAddressHomeCity = Read-Host "City"
+$PhysicalAddressHomeCountry = Read-Host "Country"
+$PhysicalAddressHomeState = Read-Host "State"
+$PhysicalAddressHomePostalCode = Read-Host "Postal Code"
+$PhysicalAddressWorkStreet = Read-Host "Work Street"
+$PhysicalAddressWorkCity = Read-Host "Work City"
+$PhysicalAddressWorkCountry = Read-Host "Work Country"
+$PhysicalAddressWorkState = Read-Host "Work State"
+$PhysicalAddressWorkPostalCode = Read-Host "Work Postal Code"
+$JobPosition = Read-Host "Work Position"
+$Company = Read-Host "Company"
 $LastModified = ""
 $belongsToAccount = $acctid1
 $isOriginal = "true"
 $isDeleted = "false"
 $relationId = ""
-Write-Host -ForegroundColor Green "<<<ZAKLADAM KONTAKT>>>"
+Write-Host -ForegroundColor Green "<<<Creating Contact>>>"
 $synctoday.CreateContact2($extid,$contactName,$PhoneNumberHome,$PhoneNumberWork,$PhoneNumberCellPhone,$EmailAddressHome,$EmailAddressWork,$Address,$PhysicalAddressHomeStreet,$PhysicalAddressHomeCity,
 $PhysicalAddressHomeCountry,$PhysicalAddressHomeState,$PhysicalAddressHomePostalCode,$PhysicalAddressWorkStreet,$PhysicalAddressWorkCity,$PhysicalAddressWorkCountry,$PhysicalAddressWorkState,
 $PhysicalAddressWorkPostalCode,$JobPosition,$Company,$LastModified,$belongsToAccount,$isOriginal,$isDeleted,$relationId)
 
-#Synchronizuj ucty
-Write-Host -ForegroundColor Yellow "<<<SYNCHRONIZUJI>>>"
+#Sync Accounts
+Write-Host -ForegroundColor Yellow "<<<Synchronizing>>>"
 $synctoday.SynchronizeUser2($userid)
-Write-Host -ForegroundColor DarkMagenta "<<<HOTOVO>>>"
+Write-Host -ForegroundColor DarkMagenta "<<<Done>>>"
